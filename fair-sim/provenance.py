95fe3248e99b587db2638f5ba0e9bb5c007d2fa2
Sarah Brown <sarahmbrown@berkeley.edu>
2018-04-24 18:31:12 -0700

    initial

---
+++ fair-sim/provenance.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def format_log(params,name):
+    """
+    create log for
+    """
+    log_info = {'function_names':name,
+                'params':params}
+
+    return log_info
