c9e3e497954b40bfbcb472077164b1d5db62a7dd
Sarah Brown <sarahmbrown@berkeley.edu>
2018-08-06 16:49:17 -0400

    remove - fro mname

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='fair-sim',
+setup(name='fairsim',
       version='0.1',
       description='synthetic data genertors for fairness assessment',
       url='http://github.com/brownsarahm/fair-sim/',
       author='Sarah M Brown',
       author_email='smb@sarahmbrown.org',
       license='MIT',
-      packages=['fair-sim'],
+      packages=['fairsim'],
       zip_safe=False,
       install_requires=['Numpy', 'Scipy'])
