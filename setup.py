9a61c9d042e4c5c780a81af9a3ab1c7324d0abd3
Sarah Brown <sarahmbrown@berkeley.edu>
2019-01-22 13:49:52 -0500

    rename

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='fairsim',
+setup(name='mlsim',
       version='0.1',
-      description='synthetic data genertors for fairness assessment',
-      url='http://github.com/brownsarahm/fair-sim/',
+      description='synthetic data generators for machine learning evaluation',
+      url='http://github.com/brownsarahm/ml-sim/',
       author='Sarah M Brown',
       author_email='smb@sarahmbrown.org',
       license='MIT',
-      packages=['fairsim'],
+      packages=['mlsim'],
       zip_safe=False,
       install_requires=['Numpy', 'Scipy'])
