7b174d7a48d9c9d1478f913e8d1575641d57b913
Sarah Brown <sarahmbrown@berkeley.edu>
2020-04-08 12:23:23 -0400

    comment label bias and make general to add any number of features

--- mlsim/bias_generators.py
+++ mlsim/bias_generators.py
@@ -8,18 +8,20 @@
 # mu - array of arrays with dimensions [a][z]
 # d_shared - int, number of shared features
 def feature_bias2(rho_a, rho_z, beta, N, d, d_shared, mu):
-    # portion of disadvantaged group  
+    '''
+    '''
+    # portion of disadvantaged group
     p_a = [1-rho_a, rho_a]
     # portion of allocation of target variable
     p_z = [1-rho_z, rho_z]
     cov = np.eye(d)
-    
+
     a = np.random.choice([0,1], p=p_a, size=N)
     z = np.random.choice([0,1], p=p_z, size=N)
     x = [np.random.multivariate_normal(mu[a_i][z_i],cov) for a_i, z_i in zip(a,z)]
 
     x = np.asarray(x)
-    
+
     y = z
     data = np.asarray([a,z,y]).T
     df = pd.DataFrame(data=data, columns = ['a','z','y'])
@@ -29,17 +31,19 @@ def feature_bias2(rho_a, rho_z, beta, N, d, d_shared, mu):
         var = 'x' + str(i)
         df[var] = x[:,i]
         var_list.append(var)
-    
+
     df.head()
-    
+
     return df
 
 def feature_bias(rho_a, rho_z, beta, N, d, d_shared, mu):
+    '''
+    '''
     p_a = [1-rho_a, rho_a]
     p_z = [1-rho_z, rho_z]
-    
+
     cov = np.eye(d)
-    
+
     d_noise = d-d_shared # noise dims per row
     d_total = d + d_noise # total dims
 
@@ -48,7 +52,7 @@ def feature_bias(rho_a, rho_z, beta, N, d, d_shared, mu):
     x_z = [np.random.multivariate_normal(mu[z_i],cov) for z_i in z]
     x_n = np.random.multivariate_normal([0]*d_noise,np.eye(d_noise),N)
         # functions for combining noise and true vectors
-    x_a = {0: lambda x,n: np.concatenate((x,n)), 
+    x_a = {0: lambda x,n: np.concatenate((x,n)),
           1: lambda x,n: np.concatenate((n, x[d_shared-1:d],  x[:d_noise]))}
     x = [x_a[a](x_zi,x_ni) for a,x_zi,x_ni in zip(a,x_z,x_n)]
     x = np.asarray(x)
@@ -67,18 +71,45 @@ def feature_bias(rho_a, rho_z, beta, N, d, d_shared, mu):
     return df
 
 def label_bias(rho_a, rho_z, beta, N, mu, cov):
+    '''
+    Bias where the labeling errors are correlated with the protected attribute
+
+
+    Parameters
+    -----------
+    rho_a : float
+        p(a = 1)
+    rho_z : float
+        p(z=1)
+    beta : float
+        error rate in y, p(y=z) = 1-beta
+    N : int
+         number of samples
+    mu : matrix like, 2xD
+        mu[0] is the mean for z=0, D= len(mu[0]) = numbe of features
+    cov : 2x2
+        covariance, shared across classes
+
+    Returns
+    --------
+    df : DataFrame
+        a data frame with N rows and columns: a,y,z, x0,x1
+
+    '''
     p_a = [1-rho_a, rho_a]
     p_z = [1-rho_z, rho_z]
-    
+
     a = np.random.choice([0,1], p=p_a, size=N)
     z = np.random.choice([0,1], p=p_z, size=N)
     x = [np.random.multivariate_normal(mu[z_i],cov) for z_i in z]
     y = [np.random.choice([zi,1-zi],p=[1-beta[ai], beta[ai]]) for ai,zi in zip(a,z)]
-    data = np.asarray([a,z,y]).T
+    # convert to numpy arrays and reshape
+    labels_protected = np.asarray([a,z,y]).T
     x = np.asarray(x)
-    df = pd.DataFrame(data=data, columns = ['a','z','y'])
-    
-    df['x0'] = x[:,0]
-    df['x1'] = x[:,1]
-    
+    # concatenate the data and p
+    data = np.concatenate([labels_protected,x],axis=1)
+    labels =['a','z','y']
+    labels.extend(['x'+str(i) for i in range(D)])
+    df = pd.DataFrame(data=data, columns = labels)
+
     return df
