8afd2a03f1fb2083e7925afa63b3929ede9a0429
Sarah Brown <sarahmbrown@berkeley.edu>
2020-04-21 15:35:48 -0400

    add more accessible

--- mlsim/__init__.py
+++ mlsim/__init__.py
@@ -2,7 +2,7 @@
 from .simpsons_paradox import geometric_2d_gmm_sp
 from .simpsons_paradox import geometric_indep_views_gmm_sp
 from .plot_utils import sp_plot, plot_clustermat
-from .bias_generators import feature_bias2, feature_bias, label_bias
+from .bias_generators import subspace_bias, feature_bias, label_bias
 
 
 __all__ = ['sp_plot', 'geometric_2d_gmm_sp',
