0190e96c54391cb502cc0e08fe86afda58d90830
paziabermudez <pazia_bermudez-silverman@brown.edu>
2020-05-13 14:48:07 -0700

    Moving simulation generators to structural racism

--- mlsim/__init__.py
+++ mlsim/__init__.py
@@ -3,8 +3,7 @@ from .simpsons_paradox import geometric_2d_gmm_sp
 from .simpsons_paradox import geometric_indep_views_gmm_sp
 from .plot_utils import sp_plot, plot_clustermat
 from .bias_generators import subspace_bias, feature_bias, label_bias, convert_to_dataset
-from .sim_gens import feature_sim, subspace_sim, label_sim
 
 
 __all__ = ['sp_plot', 'geometric_2d_gmm_sp',
-           'geometric_indep_views_gmm_sp', 'plot_clustermat', 'bias_generators', 'sim_gens']
+           'geometric_indep_views_gmm_sp', 'plot_clustermat', 'bias_generators']
