6086ce269cd367348076ea82b9c514a82a4df38e
cjmorris28 <54966702+cjmorris28@users.noreply.github.com>
2022-03-01 15:51:34 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,18 +38,12 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = ['sphinx.ext.autodoc',
-#    'sphinx.ext.intersphinx',
-#    'sphinx.ext.mathjax',
-#    'sphinx.ext.viewcode',
-#    'sphinx.ext.githubpages',
-#    'nbsphinx',
-#    'numpydoc',]
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'nbsphinx',
     'numpydoc',]
 
 # Add any paths that contain templates here, relative to this directory.
